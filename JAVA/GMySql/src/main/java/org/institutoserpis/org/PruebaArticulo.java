fbf22bf0ad8474bf613367ca4457d8c54932be3d
jmollaf <jmollaf@gmail.com>
2015-12-15 10:43:59 +0100

    CONEXION MYSQL

--- JAVA/GMySql/src/main/java/org/institutoserpis/org/PruebaArticulo.java
+++ JAVA/GMySql/src/main/java/org/institutoserpis/org/PruebaArticulo.java
@@ -1,5 +1,14 @@
 package org.institutoserpis.org;
 
-public class PruebaArticulo {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class PruebaArticulo {
+             public static void main(String[] args) throws SQLException {
+            	    Connection connection = DriverManager.getConnection(
+            	    		"jdbc:mysql://localhost/dbprueba", "root", "sistemas");
+            	    connection.close();
+            	    System.out.println("fin");
+             }
 }
